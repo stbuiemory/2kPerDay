47d245e8f96dccc43315858fae77d115fa981f9e
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-01 21:09:10 -0400

    added perenial api call information into the api file

---
+++ controllers/api/perenial.js
@@ -0,0 +1,8 @@
+/*api URL with key
+ https://perenual.com/api/species-list?page=1&key=sk-YD7M644b1a9a55fb8682
+*/
+
+/*call to the api for plant search data
+ https://perenual.com/api/species-list?page=1&key=sk-YD7M644b1a9a55fb8682&q={USER SEARCH INPUT}
+*/
+
