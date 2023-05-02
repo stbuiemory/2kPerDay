8488ef1f9565f53510dcc962e6023da87a5ebe40
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-02 15:16:35 -0400

    added perenial api call psuedo code into perenial.js

--- controllers/api/perenial.js
+++ controllers/api/perenial.js
@@ -6,3 +6,6 @@
  https://perenual.com/api/species-list?page=1&key=sk-YD7M644b1a9a55fb8682&q={USER SEARCH INPUT}
 */
 
+/*call to the api for specific plant care data
+https://perenual.com/api/species/details/{API PLANT ID}?key=sk-YD7M644b1a9a55fb8682
+*/
