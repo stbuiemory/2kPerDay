5f082d7d71ce787659423d38f74695b0247bf624
clarence_c <clarence@biteline.net>
2023-05-02 16:08:13 -0700

    change db name

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF EXISTS ourplantdatabasename_db;
-CREATE DATABASE ourplantdatabasename_db;
+DROP DATABASE IF EXISTS hotPots_db;
+CREATE DATABASE hotPots_db;
 
 USE ourplantdatabasename_db;
