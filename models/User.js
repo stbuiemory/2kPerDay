3afe7a5e4e1774b339dddc36fafcfabbe7bff2aa
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-02 20:03:33 -0400

    removed user file created by me due to another team mate creating duplicate code. Going with their code.  Renamed parent.js to user.js

--- models/User.js
+++ models/User.js
@@ -1,40 +1,84 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+// create our user model
+class user extends Model {}
 
-//initializes user model
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  /*email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },*/
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8],
+// create fields/columns for user model
+// Do we want to include a level or status, emoji/badge for points accumulated?
+user.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // must be longer than 8 characters
+      validate: {
+        len: [8],
+        isAlphanumeric: true,
+      },
+    },
+    points: {
+      type: DataTypes.INTEGER,
+      allowNull: true, // starting level at zero or blank?
+      /* users accumulate points for caring for their plants 
+      or for completing quizzes about plant knowledge 
+      points indicate user level or progress in plant parenting
+      earn badges or prizes (like fertilizer or cute labels or new garden tools) for leveling up
+      */
     },
   },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: 'user',
-});
+  {
+    // ADDING THESE HOOKS HERE FOR PRACTICE, maybe not necessary unless emails are not recognized as "unique" if they are mixed case
+    // When adding hooks via the init() method, they go below
+    hooks: {
+      // Use the beforeCreate hook to work with data before a new instance is created
+      beforeCreate: async (newParentData) => {
+        // In this case, we are taking the user's email address, and making all letters lower case before adding it to the database.
+        newParentData.email = await newParentData.email.toLowerCase();
+        return newParentData;
+      },
+      // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated email address, before updating the database.
+      beforeUpdate: async (updatedParentData) => {
+        updatedParentData.email = await updatedParentData.email.toLowerCase();
+        return updatedParentData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
 module.exports = user;
