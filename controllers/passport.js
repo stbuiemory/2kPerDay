f63375768fca023a0b283854cd2f57309a6c7c94
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-08 13:31:05 -0400

    corrected some issues with the passport.js code

--- controllers/passport.js
+++ controllers/passport.js
@@ -5,34 +5,31 @@ const localStrategy = require('passport-local').Strategy;
 
 function initialize(passport, getUserByUsername) {
   const authenticateUser = (username, password, done) => {
-    //creating user const and checking username to confirm if their is a user with this name
-    const user = getUserByUsername(Username);
+    const user = getUserByUsername(username);
 
-    //if no user with this username, returning message that there is no user with that username
     if (user === null) {
       return done(null, false, { message: 'No user with that username' });
     }
 
     try {
-      //bcrypt will compare hashed,stored password to confirm if valid
-      if (bcrypt.compare(password)) {
+      if (bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        //if not valid returning message that password is incorrect
         return done(null, false, { message: 'Password entered was incorrect' });
       }
     } catch (err) {
       return done(err);
     }
   };
-  passport.use(new localStrategy(), authenticateUser);
+
+  passport.use(new localStrategy(authenticateUser));
   passport.serializeUser((user, done) => {});
   passport.deserializeUser((id, done) => {});
 }
 
 router.post(
   '',
-  passport.authentication('local', {
+  passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
   })
@@ -47,4 +44,4 @@ router.post('/logout', function (req, res, next) {
   });
 });
 
-module.export = initialize;
+module.exports = initialize;
