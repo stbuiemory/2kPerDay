e3e73fe31debc321663fbbaad371486e9add8d5e
lillianlaihughes <lillianlaihughes@gmail.com>
2023-05-08 22:58:35 -0400

    updated plant route js

--- controllers/api/plantRoutes.js
+++ controllers/api/plantRoutes.js
@@ -1,15 +1,29 @@
 const router = require('express').Router();
 const { Plant } = require('../../models');
-
 const dayjs = require('dayjs');
 
 dayjs().format();
 
-// GET all plants
+// GET all plants and JOIN with user data
 router.get('/', async (req, res) => {
   try {
-    const plantData = await Plant.findAll();
-    res.status(200).json(plantData);
+    const plantData = await Plant.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    // Serialize data so the template can read it
+    const plants = plantData.map((plant) => plant.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('mygarden', {
+      plants,
+      logged_in: req.session.logged_in,
+    });
+    // res.status(200).json(plantData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -19,8 +33,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const plantData = await Plant.findByPk(req.params.id, {
-      // // JOIN with Location, using the User through table
-      // include: [{ model: Location, through: User, as: 'plant_locations' }],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
     });
 
     if (!plantData) {
@@ -28,25 +46,33 @@ router.get('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(plantData);
+    const plant = plantData.get({ plain: true });
+
+    res.render('viewspecificplant', {
+      ...plant,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// CREATE a single plant
-// router.post('/', async (req, res) => {
-//   try {
-//     const newPlant = await Plant.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newPlant);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+// CREATE (OR ADD) a single plant
+router.post('/', async (req, res) => {
+  try {
+    const newPlant = await Plant.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.render('viewspecificplant', {
+      ...newPlant,
+      logged_in: req.session.logged_in,
+    });
+    // res.status(200).json(newPlant);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // DELETE a single plant
 router.delete('/:id', async (req, res) => {
@@ -63,7 +89,12 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(plantData);
+    const plant = plantData.get({ plain: true });
+    // LIL TODO: Do we need to have a diff hbs for deleted plants?
+    res.render('viewspecificplant', {
+      ...plant,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
